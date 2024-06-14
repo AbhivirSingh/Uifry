a5f4786a787bbf4b5414fa2052bb6d7222b795fe
AbhivirSingh <134691023+AbhivirSingh@users.noreply.github.com>
2024-06-14 18:01:09 +0530

    made responsive

---
+++ docs/script.js
@@ -0,0 +1,32 @@
+// Download App btn
+const btn = document.querySelector('.readytostart button');
+btn.addEventListener('mouseover', () => {
+    btn.style.backgroundColor = "#f55";
+    btn.style.color = "white";
+    btn.querySelector('path').style.transition = "0.3s";
+    btn.querySelector('path').style.fill = "white";
+});
+btn.addEventListener('mouseout', () => {
+    btn.style.backgroundColor = "white";
+    btn.style.color = "black";
+    btn.querySelector('path').style.fill = "black";
+});
+
+// Grid part
+const media = () => {
+    let q = matchMedia("(max-width:1100px)");
+    const a = document.querySelectorAll('.mainfaq div');
+    if (q.matches) {
+        for (let i = 0; i < a.length; i++) {
+            if (i % 2 == 0) {
+                a[i].classList.add('div1');
+                a[i].classList.remove('div2');
+            }
+            else {
+                a[i].classList.add('div2');
+                a[i].classList.remove('div1');
+            }
+        }
+    }
+};
+window.onload = media;
